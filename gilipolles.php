2bd74149d2cddc7209aef0d75f08d1c82f0c0e61
LittleBaali <33788861+LittleBaali@users.noreply.github.com>
2017-11-25 19:09:32 +0100

    Create gilipolles.php

---
+++ gilipolles.php
@@ -0,0 +1,11 @@
+<?php
+
+<select name=”country” id=”contry”>
+<option value=”0” selected=”selected”> Search any Product.</option>
+<option value “informática”>
+<option value “moda”>
+<option value “libros”>
+<option value “música”>
+<option value “jueguetes”>
+
+?>
